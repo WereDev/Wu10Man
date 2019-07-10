55db84b78aef9d6874096a99294404350d2c9d41
WereDev <danielm@weredev.com>
2019-07-09 22:01:41 -0500

    continued work on pause, will read settings

---
+++ Wu10Man/UserControls/Models/PauseUpdatesModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace WereDev.Utils.Wu10Man.UserControls.Models
+{
+    class PauseUpdatesModel : ModelBase
+    {
+        private DateTimeOffset? _featureUpdatePauseDate = null;
+        public DateTimeOffset? FeatureUpdatePauseDate
+        {
+            get
+            {
+                return _featureUpdatePauseDate;
+            }
+            set
+            {
+                _featureUpdatePauseDate = value;
+                OnPropertyChanged(nameof(FeatureUpdatePauseDate));
+            }
+        }
+
+        private int _featureUpdateDelayDays = 0;
+        public int FeatureUpdateDelayDays
+        {
+            get
+            {
+                return _featureUpdateDelayDays;
+            }
+            set
+            {
+                _featureUpdateDelayDays = value;
+                OnPropertyChanged(nameof(FeatureUpdateDelayDays));
+            }
+        }
+
+        private DateTimeOffset? _qualityUpdatePauseDate = null;
+        public DateTimeOffset? QualityUpdatePauseDate
+        {
+            get
+            {
+                return _qualityUpdatePauseDate;
+            }
+            set
+            {
+                _qualityUpdatePauseDate = value;
+                OnPropertyChanged(nameof(QualityUpdatePauseDate));
+            }
+        }
+
+        private int _qualityUpdateDelayDays = 0;
+        public int QualityUpdateDelayDays
+        {
+            get
+            {
+                return _qualityUpdateDelayDays;
+            }
+            set
+            {
+                _qualityUpdateDelayDays = value;
+                OnPropertyChanged(nameof(QualityUpdateDelayDays));
+            }
+        }
+
+    }
+}
--- Wu10Man/UserControls/PauseUpdatesControl.xaml.cs
+++ Wu10Man/UserControls/PauseUpdatesControl.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WereDev.Utils.Wu10Man.Editors;
+using WereDev.Utils.Wu10Man.UserControls.Models;
 
 namespace WereDev.Utils.Wu10Man.UserControls
 {
@@ -20,10 +23,69 @@ namespace WereDev.Utils.Wu10Man.UserControls
     /// </summary>
     public partial class PauseUpdatesControl : UserControl
     {
+        private readonly Regex _numberOnlyRegex = new Regex("[^0-9]+");
+        private readonly PauseUpdatesModel _model = new PauseUpdatesModel();
+        private const string _registryKey = @"SOFTWARE\Microsoft\WindowsUpdate\UX\Settings";
+        private const string DeferFeatureUpdatesPeriodInDays = "DeferFeatureUpdatesPeriodInDays";
+        private const string PauseFeatureUpdatesEndTime = "PauseFeatureUpdatesEndTime";
+        private const string DeferQualityUpdatesPeriodInDays = "DeferQualityUpdatesPeriodInDays";
+        private const string PauseQualityUpdatesEndTime = "PauseQualityUpdatesEndTime";
+
         public PauseUpdatesControl()
         {
             ToolTipService.ShowDurationProperty.OverrideMetadata(typeof(DependencyObject), new FrameworkPropertyMetadata(Int32.MaxValue));
+            DataContext = _model;
             InitializeComponent();
+            ReadCurrentSettings();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = _numberOnlyRegex.IsMatch(e.Text);
+        }
+
+        private void ReadCurrentSettings()
+        {
+            _model.FeatureUpdateDelayDays = GetIntFromRegistryValue(DeferFeatureUpdatesPeriodInDays);
+            _model.FeatureUpdatePauseDate = GetDateFromRegistryValue(PauseFeatureUpdatesEndTime);
+            _model.QualityUpdateDelayDays = GetIntFromRegistryValue(DeferQualityUpdatesPeriodInDays);
+            _model.QualityUpdatePauseDate = GetDateFromRegistryValue(PauseQualityUpdatesEndTime);
+        }
+
+        private DateTimeOffset? GetDateFromRegistryValue(string registryName)
+        {
+            var registryValue = RegistryEditor.ReadLocalMachineRegistryValue(_registryKey, registryName);
+            if (DateTimeOffset.TryParse(registryValue, out var parsedValue))
+                return parsedValue;
+            else
+                return null;
+        }
+
+        private int GetIntFromRegistryValue(string registryName)
+        {
+            var registryValue = RegistryEditor.ReadLocalMachineRegistryValue(_registryKey, registryName);
+            if (int.TryParse(registryValue, out var parsedValue))
+                return parsedValue;
+            else
+                return 0;
+        }
+
+        private void SaveChanges(object sender, System.Windows.RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ResetChanges(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ReadCurrentSettings();
+        }
+
+        private void ClearValues(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _model.FeatureUpdateDelayDays = 0;
+            _model.FeatureUpdatePauseDate = null;
+            _model.QualityUpdateDelayDays = 0;
+            _model.QualityUpdatePauseDate = null;
         }
     }
 }
