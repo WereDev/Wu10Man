f4f74ab8245a3c57212ca1a2df837a64ad6fe587
WereDev <danielm@weredev.com>
2019-07-09 19:58:50 -0500

    additional os and .net version logging

--- Wu10Man/App.xaml.cs
+++ Wu10Man/App.xaml.cs
@@ -14,11 +14,11 @@ namespace WereDev.Utils.Wu10Man
             Wu10Logger.LogInfo("Application starting");
             try
             {
-                var version = this.GetType().Assembly.GetName().Version;
-                Wu10Logger.LogInfo($"Application version: v{version.ToString()}");
+                WriteStartupLogs();
                 this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
                 this.MainWindow = new MainWindow();
                 this.MainWindow.Show();
+
                 Wu10Logger.LogInfo("Application started");
             }
             catch (Exception ex)
@@ -41,6 +41,48 @@ namespace WereDev.Utils.Wu10Man
             string errorMessage = string.Format("{0}\r\n\r\nCheck the logs for more details.", e.Exception.Message);
             MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             e.Handled = true;
+        }
+
+        void WriteStartupLogs()
+        {
+            var appVersion = this.GetType().Assembly.GetName().Version;
+            Wu10Logger.LogInfo($"Application version: v{appVersion.ToString()}");
+
+            var windowsProduct = Editors.RegistryEditor.ReadLocalMachineRegistryValue(@"SOFTWARE\WOW6432Node\Microsoft\Windows NT\CurrentVersion", "ProductName");
+            var windowsVersion = Environment.OSVersion.Version.ToString();
+            Wu10Logger.LogInfo($"{windowsProduct} {windowsVersion}");
+            Wu10Logger.LogInfo($".Net Framework: {GetNetFrameworkBuild()}");
+        }
+
+        string GetNetFrameworkBuild()
+        {
+            var release = Editors.RegistryEditor.ReadLocalMachineRegistryValue(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full", "Release");
+            int.TryParse(release, out var releaseInt);
+
+            if (releaseInt >= 528040)
+                return $"{release} / 4.8 or later";
+            else if (releaseInt >= 461808)
+                return $"{release} / 4.7.2";
+            else if (releaseInt >= 461308)
+                return $"{release} / 4.7.1";
+            else if (releaseInt >= 460798)
+                return $"{release} / 4.7";
+            else if (releaseInt >= 394802)
+                return $"{release} / 4.6.2";
+            else if (releaseInt >= 394254)
+                return $"{release} / 4.6.1";
+            else if (releaseInt >= 393295)
+                return $"{release} / 4.6";
+            else if (releaseInt >= 393273)
+                return $"{release} / 4.6 RC";
+            else if ((releaseInt >= 379893))
+                return $"{release} / 4.5.2";
+            else if ((releaseInt >= 378675))
+                return $"{release} / 4.5.1";
+            else if ((releaseInt >= 378389))
+                return $"{release} / 4.5";
+            else
+                return $"{release} / No 4.5 or later version detected";
         }
     }
 }
